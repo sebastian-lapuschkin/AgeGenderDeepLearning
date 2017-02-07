bb053b378aeace94ced82ea44f1a479c6e7d633f
sebastian-lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2017-02-07 18:58:29 +0100

    gixed datagen script to follow logical sequence, create required folder structure. updated .gitignore

--- run_all_datagen_scripts.sh
+++ run_all_datagen_scripts.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-find ./*/ -name "*.sh" -exec bash '{}' \;
+#create necessary folders, since caffe tools do not create paths recursively
+mkdir lmdb
+for i in {0..4}
+do
+	mkdir lmdb/Test_fold_is_$i
+done
+
+#run lmdb creation scripts
+find ./*/ -name "create_lmdb*.sh" -exec bash '{}' \;
+
+#compute mean files
+find ./*/ -name "make_mean*.sh" -exec bash '{}' \;
