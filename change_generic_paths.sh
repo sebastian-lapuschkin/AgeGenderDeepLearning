7208055c5d1695bdde2ab8454aa5337d82ae30d6
sebastian-lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2017-02-07 18:35:27 +0100

    fixed script

--- change_generic_paths.sh
+++ change_generic_paths.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #this script replaces all generic /home/ubuntu/AdienceFaces paths to he current one in all sripts.
 
-CAFFEPATH=$(HOME)/code/caffe
+CAFFEPATH=$HOME/code/caffe
 
 #change root paths
-find ./*/ -type f -exec sed -i -e s^/home/ubuntu/AdienceFaces^$HOME^g
+find ./*/ -type f -exec sed -i -e s^/home/ubuntu/AdienceFaces^$PWD^g '{}' \;
 
-#change caffe tools path
-find ./*/ -type f -exec sed -i -e ^/home/ubuntu/repositories^$CAFFEPATH^g
+#change caffe tools path 
+find ./*/ -type f -exec sed -i -e s^/home/ubuntu/repositories^$CAFFEPATH^g '{}' \;
