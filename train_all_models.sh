6204bbca5de50bb635b16bd4dade47aeb5e100cf
sebastian-lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2017-02-09 12:37:55 +0100

    moving models after training to make room for new ones

--- train_all_models.sh
+++ train_all_models.sh
@@ -8,8 +8,8 @@ mkdir $flags
 cd gender_net_definitions
 for i in {0..4}
 do
-    #create start flag. on suceess start training. on success create done flag.
-    date >> $flags/gender_$i.started && caffe train --solver solver_test_fold_is_$i.prototxt && date >> $flags/gender_$i.done
+    #create start flag. on suceess start training. on success move model files. on success create done flag.
+    date >> $flags/gender_$i.started && caffe train --solver solver_test_fold_is_$i.prototxt && mkdir models_test_is_$i && mv *.caffemodel models_test_is_$i/ && mv *.solverstate models_test_is_$i/ && date >> $flags/gender_$i.done
 done
 
 #go back up
@@ -19,8 +19,8 @@ cd $here
 cd age_net_definitions
 for i in {0..4}
 do
-    #create start flag. on suceess start training. on success create done flag.
-    date >> $flags/age_$i.started && caffe train --solver solver_test_fold_is_$i.prototxt && date >> $flags/age_$i.done
+    #create start flag. on suceess start training. on success move model files. on success create done flag.
+    date >> $flags/age_$i.started && caffe train --solver solver_test_fold_is_$i.prototxt && mkdir models_test_is_$i && mv *.caffemodel models_test_is_$i/ && mv *.solverstate models_test_is_$i/ && date >> $flags/age_$i.done
 done
 
 cd $here
