247f4d8cb60109408c32e779feb3aeadfa0f688b
sebastian-lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2017-02-09 09:46:33 +0100

    added printed-out comments

--- change_generic_paths.sh
+++ change_generic_paths.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 #this script replaces all generic /home/ubuntu/AdienceFaces paths to he current one in all sripts.
 
+#TODO: adjust this to where your caffe is
 CAFFEPATH=$HOME/code/caffe
 
+echo ''
+echo 'Fixing Project Root Paths'
+echo ''
 #change root paths
 find ./*/ -type f -exec sed -i -e s^/home/ubuntu/AdienceFaces^$PWD^g '{}' \;
 
+echo ''
+echo 'Setting Caffe Tool Path'
+echo ''
 #change caffe tools path 
 find ./*/ -type f -exec sed -i -e s^/home/ubuntu/repositories/caffe^$CAFFEPATH^g '{}' \;
 
+echo ''
+echo 'Unifying LMDB and MEAN file naming patterns'
+echo ''
 #unify path definitions for lmdb and mean_images
 find ./*/ -type f -exec sed -i -e s^/mean_image/Test_folder_is_^/mean_image_Test_fold_is_^g '{}' \;
--- run_all_datagen_scripts.sh
+++ run_all_datagen_scripts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #create necessary folders, since caffe tools do not create paths recursively
+echo ''
+echo 'Creating Folders'
+echo ''
 mkdir lmdb
 mkdir mean_image
 for i in {0..4}
@@ -9,8 +12,15 @@ do
 	mkdir mean_image/Test_fold_is_$i
 done
 
+echo ''
+echo 'Running LMDB file creation'
+echo ''
 #run lmdb creation scripts
 find ./*/ -name "create_lmdb*.sh" -exec bash '{}' \;
 
+
+echo ''
+echo 'Running MEAN file creation'
+echo ''
 #compute mean files
 find ./*/ -name "make_mean*.sh" -exec bash '{}' \;
